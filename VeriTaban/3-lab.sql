53b1dffffd6b283fe21aeb2536a6c8ec01d8200f
Chontaev <65342023+Chontaev@users.noreply.github.com>
2021-02-17 17:13:01 +0300

    Update 3-lab.sql

--- VeriTaban/3-lab.sql
+++ VeriTaban/3-lab.sql
@@ -1,25 +1,36 @@
-CREATE TABLE users
-(
-    user_id INT(10) AUTO_INCREMENT PRIMARY KEY,
-    user_name VARCHAR(20) NOT NULL,
-    user_email VARCHAR(50) NOT NULL,
-    user_password VARCHAR(15) NOT NULL
-);
 
-CREATE TABLE topics 
-(
-    topic_id int (10) AUTO_INCREMENT, 
-    topic_name varchar(100) NOT NULL, 
-    author_id int (10) NOT NULL, 
-    PRIMARY KEY (topic_id),
-    FOREIGN KEY (author_id) REFERENCES users (user_id
- ));
-CREATE TABLE mail
-(
-    mail_id int (10) AUTO_INCREMENT PRIMARY KEY, 
-    mail_description TEXT(500) NOT NULL, 
-    author_id int (10) NOT NULL, 
-    topic_id int (10) NOT NULL,
-    FOREIGN KEY (author_id) REFERENCES users (user_id),
-    FOREIGN KEY (topic_id) REFERENCES topics (topic_id
-));
+#Поиск по Создателю категории
+
+SELECT user_name FROM users WHERE user_id =
+(SELECT author_id FROM topics WHERE topic_name='Красные носки');
+
+#Передаем ответ запроса как аргумет новому запросу 
+SELECT topic_name FROM topics WHERE topic_id IN (4,1);
+
+#2 способа поиска c помощью аргумента in и '='
+SELECT User_name FROM users WHERE user_id IN
+(SELECT author_id FROM topics WHERE topic_name='Красные носки');
+
+SELECT User_name FROM users WHERE user_id =
+(SELECT author_id FROM topics WHERE topic_name='Красные носки');
+
+#Обединенный запрос к двум таблицам
+
+SELECT user_name, user_email, user_password, topic_id, topic_name FROM users, topics;
+
+#Вывод авторов категорий
+
+SELECT topic_name, user_name From topics, users WHERE topics.author_id = users.user_id
+
+#Вывод авторов записей
+
+SELECT mail_description, user_name From mail, users WHERE mail.author_id = users.user_id
+
+#Вывод записей из трех таблиц по автору и категории
+
+SELECT mail_description, topic_name, user_name From mail, topics, users 
+WHERE mail.author_id = users.user_id AND mail.topic_id = topics.topic_id
+
+#Сортировка вывода с помощью id автора
+SELECT mail_description, topic_name, user_name From mail, topics, users 
+WHERE mail.author_id = users.user_id AND mail.topic_id = topics.topic_id and mail.author_id = 1
